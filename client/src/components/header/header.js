3d93359a121bbce4782f06662ac2884120644108
Calum Ragan <searagan@gmail.com>
2019-02-06 13:37:02 -0800

    header

---
+++ client/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+Const Header = () => (
+    <div className="header">
+        <h1>The King's Arcade</h1>
+        <h3>Reign over all players</h3>
+    </div>
+);
+
+export default Header;
