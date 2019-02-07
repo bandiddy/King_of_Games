9a0a9247f0ef1e3dbff506abc9a67fee856d820e
Calum Ragan <searagan@gmail.com>
2019-02-07 10:32:36 -0800

    more basic components

---
+++ client/src/components/Score/score.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./score.css";
+
+const Score = () => (
+ <div className="score">
+    <h2>The King and the Ursurpers!</h2>
+    <ol>
+        <li>James</li>
+        <li>Calum</li>
+        <li>Kelan</li>
+        <li>Alvin</li>
+        <li>Taylor</li>
+    </ol>
+ </div>
+);
+
+export default Score;
