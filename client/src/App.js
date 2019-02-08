378a48f8011912c8c157564f2942c4591f2f8197
alvin <alvinjoseph.garcia@gmail.com>
2019-02-08 10:16:09 -0800

    react

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,11 @@ import Navbar from './components/Navbar/Navbar'
 import games from "./games.json";
 import Wrapper from "./components/Wrapper/Wrapper"
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Snake from "./components/Pages/snake"
+import Asteroids from "./components/Pages/asteroids"
+import Tower from "./components/Pages/towerdefense"
+import Racecar from "./components/Pages/racecar"
+import Topscore from "./components/Pages/topscore"
 
 class App extends Component {
 
@@ -21,11 +26,11 @@ class App extends Component {
 <Header />
 <Navbar />
 <Route exact path="/" />
-<Route exact path="/asteroids" />
-<Route exact path="/snake" />
-<Route exact path="/towerdefense" />
-<Route exact path="/racecar" />
-<Route exact path="/topscore" />
+<Route exact path="/asteroids"  component={Asteroids} />
+<Route exact path="/snake" component={Snake}/>
+<Route exact path="/towerdefense" component={Tower}/>
+<Route exact path="/racecar" component={Racecar}/>
+<Route exact path="/topscore" component={Topscore}/>
 
 
 <Wrapper>
---
+++ client/src/components/Pages/asteroids.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Asteroids = () => (
+    <div>
+    <h1>Asteroids</h1>
+    <p>
+    <div id="asteroids"></div>
+</p>
+</div>
+    )
+    export default Asteroids;
