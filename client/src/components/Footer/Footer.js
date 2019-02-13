32bc68797b61a049f1cf33e172d9f9df0a24afc9
alvin <alvinjoseph.garcia@gmail.com>
2019-02-13 11:22:10 -0800

    new

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ export default class Footer extends React.Component {
     return (
       <footer className="footer">
         <span class="p-3 mb-2 text-white">Philly Cheese Steaks</span>
+        <img src="https://www.blimpie.com/assets/images/product/phillycheesesteak.png"  alt="philly cheese steak" class="img-rounded"></img>
       </footer>
     )
   }
