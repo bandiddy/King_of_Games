0d953febc1d660fd2fdb0f8e783f2d4dfb015526
alvin <alvinjoseph.garcia@gmail.com>
2019-02-13 10:29:01 -0800

    new

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import "./Footer.css";
 
-<<<<<<< HEAD
-const Footer = () => (
-  <footer className="footer">
-    <span class="p-3 mb-2 text-white">Philly Cheese Steaks</span>
-  </footer>
-);
-=======
 export default class Footer extends React.Component {
   render() {
     return (
@@ -17,5 +10,4 @@ export default class Footer extends React.Component {
     )
   }
 }
->>>>>>> 9cb86a88695aaaf48be96190b5b0f5c83778a690
 
--- client/src/components/GameCard/GameCard.js
+++ client/src/components/GameCard/GameCard.js
@@ -4,39 +4,14 @@ import GameImage from "../GameImage";
 import Score from "../Score"
 
 
-<<<<<<< HEAD
-const GameCard = props => (
-  <div className="card">
-    <GameInfo name={props.name} image={props.image} />
-    <div className="content">
-      <ul>
-        <li>
-          <strong> {props.name}</strong>
-        </li>
-        <li>
-          {/* <TopScore name = {props.name} score ={props.score} /> */}
-          {/* <strong>Top Score:</strong>
-        </li>
-        <li>
-          <ul>
-            <li>1:</li><strong></strong>
-            <li>2:</li><strong></strong>
-            <li>3:</li><strong></strong>
-            <li>4:</li><strong></strong>
-=======
 export default class GameCard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
->>>>>>> 9cb86a88695aaaf48be96190b5b0f5c83778a690
-
   render() {
     return(
       <div className="card">
         <GameImage name={this.props.name} image={this.props.image} />
         <div className="content">
             <h2>Game: {this.props.name}</h2>
-            <Score/>
+            <Score name={this.props.name}/>
         </div>
       </div>
     )
