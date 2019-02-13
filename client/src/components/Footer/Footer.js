2b2999cb8792b8c2531fb2a2399d492b861a6ac1
Kelan Moffatt <kelanmoffatt07@gmail.com>
2019-02-13 11:37:48 -0800

    added scenes

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import "./Footer.css";
 
-const Footer = () => (
-  <footer className="footer">
-    <span class="p-3 mb-2 text-white">Philly Cheese Steaks</span>
-  </footer>
-);
-
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <span class="p-3 mb-2 text-white">Philly Cheese Steaks</span>
+      </footer>
+    )
+  }
+}
