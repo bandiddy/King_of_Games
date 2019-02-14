e26cb099a2cd63ee968872b298876fe32aedcd9c
Calum Ragan <searagan@gmail.com>
2019-02-14 10:23:38 -0800

    presentation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import games from "./games.json";
+import Wrapper from "./components/Wrapper/Wrapper";
+import GameCard from './components/GameCard/GameCard';
+import TopScoreCard from "./components/TopScoreCard/TopScoreCard";
+import Snake from "./components/Snake/Snake";
+import Breakout from "./components/Breakout/Breakout";
+//import Tower from "./components/Towerdefense/Towerdefense";
+import Racer from "./components/Racer/Racer";
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import Login from './components/Login'
+import Modal from './components/Modal/Modal';;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-  this.state = {
-    username: "",
-    email: "",
-    password: ""
+    this.state = {
+      games,
+      username: "",
+      email: "",
+      password: ""
+    };
   };
-};
 
 
   render() {
@@ -21,7 +31,38 @@ export default class App extends Component {
       <Router>
         <div>
           <Header />
-         <Login username={this.state.username} email={this.state.email} password={this.state.password} />
+          <Navbar />
+         <Modal username={this.state.username} email={this.state.email} password={this.state.password} />
+          <Route exact path="/" />
+          <Route
+            path='/breakout'
+            name="Breakout"
+            username = {this.state.username}
+            render={(props) => <Breakout username = {this.state.username}/>}
+          />
+          <Route
+            path='/snake'
+            name="Snake"
+            username = {this.state.username}
+            render={(props) => <Snake username = {this.state.username}/>}
+          />
+          <Route
+            path='/racer'
+            name="Racer"
+            username = {this.state.username}
+            render={(props) => <Racer username = {this.state.username}/>}
+          />
+          <Wrapper>
+            <TopScoreCard/>
+            {this.state.games.map(game => (
+              <GameCard
+                id={game.id}
+                key={game.id}
+                name={game.name}
+                image={game.image}
+              />
+            ))}
+          </Wrapper>
           <Footer />
         </div>
       </Router>
--- client/src/components/Score/score.js
+++ client/src/components/Score/score.js
@@ -1,45 +1,45 @@
 import React, { Component } from "react";
 import "./Score.css";
 import API from "../../utils/API"
-
+​
 export default class Score extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          error: null,
-          isLoaded: false,
-          items: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+     error: null,
+     isLoaded: false,
+     scores: []
+    };
+   }
+  
+   componentDidMount() {
+    API.getGameScores(this.props.name)
+     .then(
+      (result) => {
+       this.setState({
+        isLoaded: true,
+        scores: result.data
+       });
+      },
+      (error) => {
+       this.setState({
+        isLoaded: true,
+        error
+       });
       }
-    
-      componentDidMount() {
-        API.getGameScores(this.props.name)
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                items: result.items
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
-    
-    render() {
-        return (
-            <ul>
-                {this.state.items.map(item => (
-                  <li key={item.username}>
-                      {item.username} {item.score}
-                  </li>
-                ))}
-            </ul>
-        )
-    }
+     )
+   }
+  
+  render() {
+   console.log(this.state.scores);
+    return (
+      <ul>
+        {this.state.scores.map(score => (
+         <li key={score.username}>
+           {score.username} {score.score}
+         </li>
+        ))}
+      </ul>
+    )
+  }
 }
