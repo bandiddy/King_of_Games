aef34c4f0c5f8d99db1425d83d9516f824af9f18
Kelan Moffatt <kelanmoffatt07@gmail.com>
2019-02-14 09:58:04 -0800

    hi

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import "./Login.css";
 
 class Login extends Component {
+  // Setting the component's initial state
+  state = {
+    username: "",
+    password: ""
+  };
 
-   handleFormSubmit = event => {
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
     
