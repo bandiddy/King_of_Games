0f427842186cd3301dac4e0d6294738d9a8bba65
Calum Ragan <searagan@gmail.com>
2019-02-18 12:02:43 -0800

    merge

--- client/src/components/Score/score.js
+++ client/src/components/Score/score.js
@@ -44,3 +44,45 @@ export default class Score extends React.Component {
     )
   }
 }
+=======
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          scores: []
+        };
+      }
+    
+      componentDidMount() {
+        API.getGameScores(this.props.name)
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                scores: result.data
+              });
+            },
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          )
+      }
+    
+    render() {
+      console.log(this.state.scores);
+        return (
+            <ul>
+                {this.state.scores.map(score => (
+                  <li key={score.username}>
+                      {score.username} {score.score}
+                  </li>
+                ))}
+            </ul>
+        )
+    }
+}
+>>>>>>> jc
