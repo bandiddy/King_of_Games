3aa26f3cbe533a99ec03f0e2ea203b20bfa95c38
Kelan Moffatt <kelanmoffatt07@gmail.com>
2019-02-21 14:23:14 -0800

    workinnng

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import Header from './components/Header/Header';
 import Signup from './components/Signup/Signup';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login'
-import axios from "axios";
 
 export default class App extends Component {
-    constructor(props) {
-      super(props)
-  
-      this.state = {
-        email:"",
-        username:"",       
-        password:""
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: ""
     }
-  // handleSubmit = async event => {
-  //   event.preventDefault();
-  //   this.loginUser(this.state.username, this.state.password);
-  // }
-
-  loginUser(username, password) {
-    axios.post("/api/users/login", {
-      username: username,
-      password: password
-    }).then(function (data) {
-      window.location.replace(data);
-    }).catch(function (err) {
-      console.log(err);
-    });
   }
 
+  onChange(field, value) {
+    this.setState({[field]: value});
+  }
 
   render() {
     return (
       <Router>
-       
         <div>
-        <Header/>
           <ul>
             <li><Link to="/">Login</Link></li>
             <li><Link to="/signup">Signup</Link></li>
@@ -49,19 +32,17 @@ export default class App extends Component {
 
           <Route exact path="/" 
             render={(props) => <Login
-              // handleSubmit={this.handleSubmit.bind(this)}
-              handler = {this.handler} />}
+              onChange={this.onChange.bind(this)}
+              email={this.state.email}
+              username={this.state.username}
+              password={this.state.password} />}
           />
           <Route path="/signup" component={Signup} />
           <Route
             path='/home'
-            // email={this.state.email}
-            // username={this.state.username}
-            // password={this.state.password}
+
             render={(props) => <Home
-              email={this.state.email}
-              username={this.state.username}
-              password={this.state.password} />}
+              handler = {this.handler} />}
           />
         </div>
       </Router>
